4c9f9b26e36d30a37ad1cebc7190e0933c31a2e6
Ignissen <jozsefpetrus99@gmail.com>
2019-10-08 12:28:00 +0200

    Collatz-length

---
+++ collatz.rkt
@@ -0,0 +1,35 @@
+#lang racket
+
+(define (collatz a l)
+  (if (= a 1)
+      l
+      (if (= (modulo a 2) 0)
+          (collatz (/ a 2) (+ l 1))
+          (collatz (+ (* a 3) 1) (+ l 1))
+          )
+      )
+  )
+
+(define (collatz-call a) (collatz a 1))
+(define collatz2 (lambda (a l)
+                  (if (= a 1)
+                      l
+                      (if (= (modulo a 2) 0)
+                          (collatz2 (/ a 2) (+ l 1))
+                          (collatz2 (+ (* a 3) 1) (+ l 1))
+                          )
+                      )
+                  )
+  )
+
+(letrec ([collatz3 (lambda (a l)
+                  (if (= a 1)
+                      l
+                      (if (= (modulo a 2) 0)
+                          (collatz3 (/ a 2) (+ l 1))
+                          (collatz3 (+ (* a 3) 1) (+ l 1))
+                          )
+                      )
+                  )])
+  (collatz3 23 1)
+  )
