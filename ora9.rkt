2d57e8f0e1d775499723ef5d7a4384848ad89713
ignissen <jozsefpetrus99@gmail.com>
2019-11-19 17:12:40 +0100

    progress

--- ora9.rkt
+++ ora9.rkt
@@ -1,18 +1,22 @@
 #lang racket
 
-(define ht (make-hash))
+
 
 ;listából dictionary-t/hash táblát készít
 (define (list->hash lst)
-  (cond
-    [(null? lst) "Got empty list. Returning..."]
-    [else
-     (hash-set! ht (caar lst) (cdar lst))
-     (list->hash (cdr lst))
-     ]
+  (define ht (make-hash))
+  (define (lst->hsh lst)
+    (cond
+      [(null? lst) "Got empty list. Returning..."]
+      [else
+       (hash-set! ht (caar lst) (cdar lst))
+       (lst->hsh (cdr lst))
+       ]
+      )
     )
+  (lst->hsh lst)
+  ht
   )
 
-(list->hash (list '(1 2 3) '(2 4) '(4 3 5) '(3 5 6 9)));
-
-ht;printing the hash table to console
+(list->hash (list '(1 2 3) '(2 4) '(4 3 5) '(3 5 6 9)) )
+(list->hash (list '(1 7 9) '(2 5 7) '(5 7 9) '(9)) )
