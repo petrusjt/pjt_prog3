1664692b2f1fd8717bf992c75b60573815158992
ignissen <jozsefpetrus99@gmail.com>
2019-11-26 17:40:06 +0100

    progress

--- ora11.rkt
+++ ora11.rkt
@@ -2,14 +2,50 @@
 
 
 (define (dividers n)   
-    (define (osztoi n i)
+    ;(define num n)
+    (define (osztoi i)
         (cond
         [(= n i) '()]
-        [(= (modulo n i) 0) (append (list i) (osztoi n (add1 i)))]
-        [else (append '() (osztoi n (add1 i)))]
+        [(= (modulo n i) 0) (append (list i) (osztoi (add1 i)))]
+        [else (append '() (osztoi (add1 i)))]
         )
     )
-    (osztoi n 1)
+    (osztoi 1)
 )
 
-(dividers 20)
+(define (sum-list lst)
+    (cond
+    [(null? lst) 0]
+    [else (+ (car lst) (sum-list (cdr lst)))]
+    )
+    
+)
+
+(sum-list (dividers 284))
+(sum-list (dividers 220))
+
+;======================================================================================================================
+
+
+;closure - beágyozott belső függvényhez rögzítünk külső értékeket
+
+;ebben a példában a g-hez az X értéket
+
+(define (f x)
+    (define (g y)
+        (+ x y)
+    )
+    g
+)
+
+(f 5)
+((f 5) 20)
+
+
+
+(define (dividers2 n)
+    (define lst (range 1 (- n 1)))
+    (filter (lambda (i) (= (modulo n i)) 0) lst)
+)
+
+(dividers2 20)
